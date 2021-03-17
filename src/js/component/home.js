76cd142aec23bc5dcee5b2417026ebb5076a2639
rroussmolina <rroussmolina@gmail.com>
2021-03-17 05:23:42 +0000

    new file

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	let [lista, setLista] = useState([]);
+
+	const addTarea = tarea => {
+		if (tarea.key === "Enter") {
+			setLista([...lista, tarea.target.value]);
+		}
+	};
+
+	//let remove = lista.splice(0, 1);
+
+	const items = [];
+
+	for (const [index, value] of lista.entries()) {
+		items.push(
+			<li className="list-group-item" key={index}>
+				<button
+					className="btn btn-default btn-xs pull-right remove-item float:right"
+					onClick={remove}>
+					<i className="fas fa-trash-alt text-danger mx-2"></i>
+				</button>
+				{value}
+			</li>
+		);
+	}
+
+	const remove = () => {
+		lista.pop();
+		console.log("removed");
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container">
+				<div className="row">
+					<div className="col-12 col-md-6">
+						<h6 className="text-muted">To Do List</h6>
+						<input
+							id="tarea"
+							onKeyDown={addTarea}
+							className="form-control"
+							type="text"
+							placeholder="What needs to be done?"></input>
+						<br />
+						<ul className="list-group">{items}</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
