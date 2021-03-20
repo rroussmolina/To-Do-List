35c7c3839aa61b08edc865b1b52680cc7c57d9b8
rroussmolina <rroussmolina@gmail.com>
2021-03-20 05:33:08 +0000

    changes

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,29 +1,104 @@
 import React, { useState, useEffect } from "react";
 
-export function Home() {
+export const TodoApp = () => {
 	let [lista, setLista] = useState([]);
+
+	useEffect(() => {
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/marichaljoshua",
+			{
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		).then(resp => {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/marichaljoshua",
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				}
+			)
+				.then(resp => {
+					console.log("respuesta", resp);
+					return resp.json();
+				})
+				.then(data => {
+					setLista(data);
+				})
+				.catch(error => {
+					//manejo de errores
+					console.log(error);
+				});
+		});
+	}, []);
 
 	const addTarea = tarea => {
 		if (tarea.key === "Enter") {
-			setLista([...lista, tarea.target.value]);
+			let newObj = {
+				label: tarea.target.value,
+				done: false
+			};
+			setLista([...lista, newObj]);
 			tarea.target.value = "";
 		}
 	};
 
 	function delTarea(pos) {
-		const tempLista = [...lista];
-		tempLista.splice(pos, 1);
-		setLista(tempLista);
+		const tempList = [...lista];
+		tempList.splice(pos, 1);
+		const methods = ["PUT", "DELETE"];
+		if (tempList.length > 0) {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/manulabarca",
+				{
+					method: methods[0],
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(tempList)
+				}
+			)
+				.then(resp => {
+					setLista(tempList);
+				})
+				.catch(error => {
+					console.log("Error delete", error);
+				});
+		} else {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/manulabarca",
+				{
+					method: methods[1],
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(tempList)
+				}
+			)
+				.then(resp => {
+					console.log("Respuesta de borrado", resp);
+					setLista(tempList);
+					console.log(lista);
+				})
+				.catch(error => {
+					console.log("Error delete", error);
+				});
+		}
 	}
 
-	const newLista = lista.map((value, index) => (
+	const newList = lista.map((value, index) => (
 		<li className="list-group-item" key={index}>
+			{value.label}
 			<button
 				onClick={() => delTarea(index)}
-				className="btn btn-default btn-xs pull-right remove-item">
+				className="btn btn-default remove-item float-end">
 				<i className="fas fa-trash-alt text-danger mx-2"></i>
 			</button>
-			{value}
 		</li>
 	));
 
@@ -39,11 +114,14 @@ export function Home() {
 							type="text"
 							placeholder="Add a thing"></input>
 						<br />
-						<ul className="list-group">{newLista}</ul>
+						<ul className="list-group">{newList}</ul>
 					</div>
 				</div>
-				<p>Total Items: {lista.length}</p>
 			</div>
+			<p>Total Items: {lista.length}</p>
+			<button className="btn btn-info">
+				<i className="fa fa-trash"></i> Clear List
+			</button>
 		</div>
 	);
-}
+};
